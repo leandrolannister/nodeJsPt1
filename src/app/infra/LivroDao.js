9e51eb83f2f6c7289b30837b182cbcee7c800db1
leandrolannister <leandrohendrix@gmail.com>
2020-09-25 07:59:22 -0300

    Gravar dados no banco

--- src/app/infra/LivroDao.js
+++ src/app/infra/LivroDao.js
@@ -3,14 +3,13 @@ const { ___resolveComponentKey } = require("marko/src/components/renderer");
 class LivroDao{
   
   constructor(db){
-   this._db = db;
-   this._query = "SELECT * FROM LIVROS";
+   this._db = db;   
   }
 
   lista(){
+    let query = "SELECT * FROM LIVROS";
     return new Promise((resolve, reject) => {
-      this._db.all(
-        this._query,
+      this._db.all(query,
         (error, result) => { 
           if (error){
             return reject('Não foi possível listar os livros');
@@ -19,7 +18,28 @@ class LivroDao{
         }  
       )            
     });
-  }  
+  } 
+  
+  store(livro) {
+    let query = `INSERT INTO livros(
+      titulo,
+      preco,
+      descricao
+    ) values(?,?,?)`;
+
+    return new Promise((resolve, reject) => {
+      this._db.run(query ,[
+        livro.titulo,
+        livro.preco,
+        livro.descricao
+      ], (erro, result) => {
+        if(erro){
+          return reject('Não foi possível listar os livros')
+        }
+        return resolve(result);
+      });      
+    });
+  }
 }
 
 module.exports = LivroDao;
